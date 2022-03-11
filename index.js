816c2e87ee8c1015197a865002b5348e23d79c78
Yalçıncan Ulus <yalcincanulus@gmail.com>
2022-03-11 15:56:04 +0300

    Add digit button functionality
    Numbers can be written and displayed on the screen.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const editScreen = document.querySelector(".screen--edit");
+const resultScreen = document.querySelector(".screen--result");
+const keypad = document.querySelector(".keypad");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -12,19 +18,30 @@ const divide = function (a, b) {
 const operate = function (operator, a, b) {
   let result;
   switch (operator) {
-    case "plus":
+    case "add":
       result = add(a, b);
       break;
-    case "minus":
+    case "subtract":
       result = subtract(a, b);
       break;
-    case "times":
+    case "multiply":
       result = multiply(a, b);
       break;
-    case "slash":
+    case "divide":
       result = divide(a, b);
       break;
   }
 
   return result;
 };
+
+let displayValue = "";
+
+keypad.addEventListener("click", function (e) {
+  if (e.target.classList.contains("key") && e.target.dataset.type === "digit") {
+    let key = e.target.dataset.key;
+    if (key === "." && displayValue.includes(".")) return;
+    displayValue = displayValue.concat(key);
+    editScreen.textContent = displayValue;
+  }
+});
